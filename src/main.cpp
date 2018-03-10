0ca5c32f5747c212ab252569e3b48cfc359a32eb
Alexander Chumakov <w5292c@gmail.com>
2018-03-10 16:43:13 +0300

    Create meeting response from meeting invitation

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,10 @@
 #include <QCoreApplication>
 
 //#define LIST_BUILTIN_TIMEZONES
-#define CREATE_RU_TIMEZONE
-#define CREATE_BERN_TIMEZONE
-#define HANDLING_DATES_TIMES
+#define MEETING_REPLY
+//#define CREATE_RU_TIMEZONE
+//#define CREATE_BERN_TIMEZONE
+//#define HANDLING_DATES_TIMES
 
 #if 0
 typedef struct _MSSystemTime {
@@ -51,6 +52,7 @@ int main(int argc, char **argv)
 
   int result = 0;
 
+#if 0
   KCalCore::MSTimeZone msTimezone;
   msTimezone.Bias = 180;
   msTimezone.StandardBias = 0;
@@ -66,6 +68,7 @@ int main(int argc, char **argv)
   icaltimezone *const icaltz = icaltimezone_get_utc_timezone();
 //timezone.icalTimezone();
   icalcomponent *const tzComponent = icaltimezone_get_component(icaltz);
+#endif /* 0 */
 
 #if 0
   /* Update standard component */
@@ -85,12 +88,12 @@ int main(int argc, char **argv)
       icalcomponent_remove_component(tzComponent, daylight1);
     }
   }
-#endif
 
   char *const resultStr = icalcomponent_as_ical_string(tzComponent);
 
   printf("Hello there:\n[%s]\n", resultStr);
   free(resultStr);
+#endif /* 0 */
 
 #ifdef LIST_BUILTIN_TIMEZONES
   /* Built-in timezones */
@@ -251,5 +254,261 @@ static const char TheExpectedBernTimezoneString[] = "BEGIN:VTIMEZONE\r\n"
   icaltimezone_free(timezoneForTime, true);
 #endif /* HANDLING_DATES_TIMES */
 
-  return 0;
+#ifdef MEETING_REPLY
+  printf("************************ CREATE REPLY ************************\n");
+static const char TheRequest[] = "BEGIN:VCALENDAR\r\n"
+"METHOD:REQUEST\r\n"
+"PRODID:Microsoft Exchange Server 2010\r\n"
+"VERSION:2.0\r\n"
+"BEGIN:VTIMEZONE\r\n"
+"TZID:(UTC+01:00) Amsterdam\\, Berlin\\, Bern\\, Rome\\, Stockholm\\, Vienna\r\n"
+"BEGIN:STANDARD\r\n"
+"DTSTART:16010101T030000\r\n"
+"TZOFFSETFROM:+0200\r\n"
+"TZOFFSETTO:+0100\r\n"
+"RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=10\r\n"
+"END:STANDARD\r\n"
+"BEGIN:DAYLIGHT\r\n"
+"DTSTART:16010101T020000\r\n"
+"TZOFFSETFROM:+0100\r\n"
+"TZOFFSETTO:+0200\r\n"
+"RRULE:FREQ=YEARLY;INTERVAL=1;BYDAY=-1SU;BYMONTH=3\r\n"
+"END:DAYLIGHT\r\n"
+"END:VTIMEZONE\r\n"
+"BEGIN:VEVENT\r\n"
+"ORGANIZER;CN=Chumakov Alexander:MAILTO:w5292c@outlook.com\r\n"
+"ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=Chumakov A\r\n"
+" lexander:MAILTO:w5292c@outlook.com\r\n"
+"ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=Челов\r\n"
+" ек Тестовый:MAILTO:w5292c.ex2@gmail.com\r\n"
+"ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=NEEDS-ACTION;RSVP=TRUE;CN=alexander.\r\n"
+" chumakov@harman.com:MAILTO:alexander.chumakov@harman.com\r\n"
+"DESCRIPTION;LANGUAGE=ru-RU:\\n\r\n"
+"UID:040000008200E00074C5B7101A82E0080000000030FE6485D7B6D301000000000000000\r\n"
+" 0100000004047422450593442A34B9832B03B232D\r\n"
+"SUMMARY;LANGUAGE=ru-RU:London time event A1\r\n"
+"DTSTART;TZID=\"(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna\":2\r\n"
+" 0180401T190000\r\n"
+"DTEND;TZID=\"(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna\":201\r\n"
+" 80401T193000\r\n"
+"CLASS:PUBLIC\r\n"
+"PRIORITY:5\r\n"
+"DTSTAMP:20180308T121809Z\r\n"
+"TRANSP:OPAQUE\r\n"
+"STATUS:CONFIRMED\r\n"
+"SEQUENCE:0\r\n"
+"LOCATION;LANGUAGE=ru-RU:Location A1\r\n"
+"X-MICROSOFT-CDO-APPT-SEQUENCE:0\r\n"
+"X-MICROSOFT-CDO-OWNERAPPTID:2116240176\r\n"
+"X-MICROSOFT-CDO-BUSYSTATUS:TENTATIVE\r\n"
+"X-MICROSOFT-CDO-INTENDEDSTATUS:BUSY\r\n"
+"X-MICROSOFT-CDO-ALLDAYEVENT:FALSE\r\n"
+"X-MICROSOFT-CDO-IMPORTANCE:1\r\n"
+"X-MICROSOFT-CDO-INSTTYPE:0\r\n"
+"X-MICROSOFT-DONOTFORWARDMEETING:FALSE\r\n"
+"X-MICROSOFT-DISALLOW-COUNTER:FALSE\r\n"
+"X-MICROSOFT-LOCATIONDISPLAYNAME:Location A1\r\n"
+"X-MICROSOFT-LOCATIONSOURCE:None\r\n"
+"BEGIN:VALARM\r\n"
+"DESCRIPTION:REMINDER\r\n"
+"TRIGGER;RELATED=START:-PT15M\r\n"
+"ACTION:DISPLAY\r\n"
+"END:VALARM\r\n"
+"END:VEVENT\r\n"
+"END:VCALENDAR\r\n";
+  static const char myEmail[] = "w5292c.ex2@gmail.com";
+
+  /* EVENT elements */
+  icalproperty *uidProperty = NULL;
+  icalproperty *dtendProperty = NULL;
+  icalproperty *transpProperty = NULL;
+  icalproperty *dtstartProperty = NULL;
+  icalproperty *dtstampProperty = NULL;
+  icalproperty *summaryProperty = NULL;
+  icalproperty *locationProperty = NULL;
+  icalproperty *attendeeProperty = NULL;
+  icalproperty *organizerProperty = NULL;
+  QList<icalproperty *> xpropertiesList;
+  /* VCALENDAR elements */
+  icalcomponent *replyComponent = NULL;
+  icalcomponent *replyEventComponent = NULL;
+  const icalproperty *prodidProperty = NULL;
+  const icalproperty *methodProperty = NULL;
+  const icalproperty *versionProperty = NULL;
+  icalcomponent *eventComponent = NULL;
+  icalcomponent *timezoneComponent = NULL;
+  icalcomponent *const requestComponent = icalparser_parse_string(TheRequest);
+  assert(requestComponent);
+  icalcomponent *currentComponent = NULL;
+  for (currentComponent = icalcomponent_get_first_component(requestComponent, ICAL_ANY_COMPONENT);
+       currentComponent;
+       currentComponent = icalcomponent_get_next_component(requestComponent, ICAL_ANY_COMPONENT)) {
+    switch (icalcomponent_isa(currentComponent)) {
+    case ICAL_VEVENT_COMPONENT:
+      eventComponent = currentComponent;
+      break;
+    case ICAL_VTIMEZONE_COMPONENT:
+      timezoneComponent = currentComponent;
+      break;
+    default:
+      printf("Unknown component: \"%s\"\n", icalcomponent_kind_to_string(icalcomponent_isa(currentComponent)));
+      break;
+    }
+  }
+  icalproperty *currentProperty = NULL;
+  for (currentProperty = icalcomponent_get_first_property(requestComponent, ICAL_ANY_PROPERTY);
+       currentProperty;
+       currentProperty = icalcomponent_get_next_property(requestComponent, ICAL_ANY_PROPERTY)) {
+    switch (icalproperty_isa(currentProperty)) {
+    case ICAL_PRODID_PROPERTY:
+      prodidProperty = currentProperty;
+      break;
+    case ICAL_METHOD_PROPERTY:
+      methodProperty = currentProperty;
+      break;
+    case ICAL_VERSION_PROPERTY:
+      versionProperty = currentProperty;
+      break;
+    default:
+      printf("Unknown property in request: \"%s\"\n", icalproperty_kind_to_string(icalproperty_isa(currentProperty)));
+      break;
+    }
+  }
+
+  if (methodProperty) {
+    const char *const methodStr = icalproperty_get_value_as_string(methodProperty);
+    if (!strcmp(methodStr, "REQUEST")) {
+      printf("Info: method in request: \"%s\"\n", methodStr);
+    } else {
+      printf("Fatal: wrong method in request: \"%s\"\n", methodStr);
+      goto cleanup;
+    }
+  } else {
+    printf("Fatal: no method element in request\n");
+    goto cleanup;
+  }
+  if (versionProperty) {
+    printf("Info: Request ICAL version: \"%s\"\n", icalproperty_get_value_as_string(versionProperty));
+  } else {
+    printf("Warning: no version element in request\n");
+  }
+  if (prodidProperty) {
+    printf ("Info: Our requestor: \"%s\"\n", icalproperty_get_value_as_string(prodidProperty));
+  }
+  if (timezoneComponent) {
+    printf("Info: timezone in request: [%s]\n", icalcomponent_as_ical_string((icalcomponent *)timezoneComponent));
+  }
+
+  /* Parse EVENT component */
+  if (!eventComponent) {
+    fprintf(stderr, "Fatal: no EVENT component\n");
+    goto cleanup;
+  }
+  /* Parsing the event comonent */
+  currentProperty = NULL;
+  for (currentProperty = icalcomponent_get_first_property(eventComponent, ICAL_ANY_PROPERTY);
+       currentProperty;
+       currentProperty = icalcomponent_get_next_property(eventComponent, ICAL_ANY_PROPERTY)) {
+    const icalproperty_kind kind = icalproperty_isa(currentProperty);
+    switch (kind) {
+    case ICAL_UID_PROPERTY:
+      uidProperty = currentProperty;
+      break;
+    case ICAL_DTSTART_PROPERTY:
+      dtstartProperty = currentProperty;
+      break;
+    case ICAL_DTEND_PROPERTY:
+      dtendProperty = currentProperty;
+      break;
+    case ICAL_DTSTAMP_PROPERTY:
+      dtstampProperty = currentProperty;
+      break;
+    case ICAL_ORGANIZER_PROPERTY:
+      organizerProperty = currentProperty;
+      break;
+    case ICAL_LOCATION_PROPERTY:
+      locationProperty = currentProperty;
+      break;
+    case ICAL_SUMMARY_PROPERTY:
+      summaryProperty = currentProperty;
+      break;
+    case ICAL_TRANSP_PROPERTY:
+      transpProperty = currentProperty;
+      break;
+    case ICAL_ATTENDEE_PROPERTY: {
+      const char emailPrefix[] = "mailto:";
+      const char *const value = icalproperty_get_value_as_string(currentProperty);
+      printf("Attendee: [%s]\n", value);
+      if (!strncasecmp(value, emailPrefix, sizeof (emailPrefix) - 1) &&
+          !strcasecmp(value + sizeof (emailPrefix) - 1, myEmail)) {
+        attendeeProperty = currentProperty;
+      }
+      }
+      break;
+    case ICAL_X_PROPERTY:
+      xpropertiesList.append(currentProperty);
+      printf("Here goes new event xproperty: [%s]\n", icalproperty_get_x_name(currentProperty));
+      break;
+    default:
+      printf("Here goes new event property: [%s]\n", icalproperty_kind_to_string(icalproperty_isa(currentProperty)));
+      break;
+    }
+  }
+  for (currentComponent = icalcomponent_get_first_component(eventComponent, ICAL_ANY_COMPONENT);
+       currentComponent;
+       currentComponent = icalcomponent_get_next_component(eventComponent, ICAL_ANY_COMPONENT)) {
+    printf("Here goes new event component: [%s]\n", icalcomponent_kind_to_string(icalcomponent_isa(currentComponent)));
+  }
+
+  /* Create the reply */
+  do {
+    /* Create the EVENT component */
+    replyEventComponent = icalcomponent_new(ICAL_VEVENT_COMPONENT);
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(dtstartProperty));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(dtendProperty));
+    icaltimetype timeNowUtc = icaltime_current_time_with_zone(icaltimezone_get_utc_timezone());
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_dtstamp(timeNowUtc));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(organizerProperty));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(uidProperty));
+    {
+    /** @todo Add attendee property here */
+    icalproperty *const me = icalproperty_new_clone(attendeeProperty);
+    icalproperty_remove_parameter(me, ICAL_RSVP_PARAMETER);
+    icalproperty_remove_parameter(me, ICAL_PARTSTAT_PARAMETER);
+    icalproperty_add_parameter(me, icalparameter_new_partstat(ICAL_PARTSTAT_ACCEPTED));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(me));
+    }
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(dtstampProperty));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_lastmodified(timeNowUtc));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(locationProperty));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_sequence(0));
+    icalcomponent_set_status(replyEventComponent, ICAL_STATUS_CONFIRMED);
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(summaryProperty));
+    icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(transpProperty));
+    foreach (icalproperty *xprop, xpropertiesList) {
+      icalcomponent_add_property(replyEventComponent, icalproperty_new_clone(xprop));
+    }
+
+    /* Create the high-level VCALENDAR component */
+    replyComponent = icalcomponent_new(ICAL_VCALENDAR_COMPONENT);
+    icalcomponent_add_property(replyComponent, icalproperty_new_method(ICAL_METHOD_REPLY));
+    icalcomponent_add_property(replyComponent, icalproperty_new_prodid("Sailfish OS, version 1.0"));
+    icalcomponent_add_property(replyComponent, icalproperty_new_version("2.0"));
+    icalcomponent_add_component(replyComponent, icalcomponent_new_clone(timezoneComponent));
+    icalcomponent_add_component(replyComponent, replyEventComponent);
+
+    printf("******************************************* REPLY *******************************************\n");
+    char *const replyStr = icalcomponent_as_ical_string(replyComponent);
+    printf("Info: generated reply:[%s]\n", replyStr);
+    free(replyStr);
+  } while (false);
+
+cleanup:
+  if (replyComponent) {
+    icalcomponent_free(replyComponent);
+  }
+  icalcomponent_free(requestComponent);
+#endif /* MEETING_REPLY */
+
+  return result;
 }
